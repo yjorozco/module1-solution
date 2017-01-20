08184e94a46912cb01bd361e89becf266c7fd8d5
yjorozco <yamiloro@gmail.com>
2017-01-20 03:36:39 -0430

    add app.js angular libraries finish basic program

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 (function () {
 'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
 
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Yaakov";
-  $scope.stateOfBeing = "hungry";
-
-  $scope.sayMessage = function () {
-    return "Yaakov likes to eat healthy snacks at night!";
-  };
-
-  $scope.feedYaakov = function () {
-    $scope.stateOfBeing = "fed";
-  };
-}
-
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    String.prototype.isEmpty = function() {
+      return (this.length === 0 || !this.trim());
+    };
+    $scope.checkLunch = function(){
+      if($scope.menu!=undefined&&!$scope.menu.isEmpty()){
+        var count = 0;
+        var elements = $scope.menu.split(",");
+        for(var i=0; i<elements.length;i++){
+          if(!elements[i].isEmpty()){
+            count++;
+          }
+        }
+        if(count>0&&count<=3){
+          $scope.message = "Enjoy!";
+        }else{
+          $scope.message = "Too much!";
+        }
+      }else{
+         $scope.message = "Please enter data first";
+      }
+    }
+  }
 })();
